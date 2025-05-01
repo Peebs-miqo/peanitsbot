7c94e7a2a6c0797bb4bf3128534690c0f78cbe6f
Peebs <68741770+Peebs-miqo@users.noreply.github.com>
2025-05-01 19:49:58 +1000

    updates

--- RaidPlannerModule.cs
+++ RaidPlannerModule.cs
@@ -21,7 +21,7 @@ public class RaidPlannerModule
 		Program.SetData(this.schedule, "schedule.json");
 	}
 
-	[SlashCommand("poll", "Start a weekly planning")]
+	[SlashCommand("poll", "Start a weekly raid time poll")]
 	public async Task Poll()
 	{
 		await this.RespondAsync("Done", ephemeral: true);
@@ -39,13 +39,7 @@ public class RaidPlannerModule
 		await this.RespondAsync("Done", ephemeral: true);
 	}
 
-	[ComponentInteraction("poll-show-me")]
-	public async Task OnShowMeCallback()
-	{
-		await this.Context.Interaction.RespondAsync(null, components:this.schedule.BuildReply(this.Context.Interaction.User.Id), ephemeral:true);
-	}
-
-	[ComponentInteraction("vote-*")]
+	[ComponentInteraction("poll-header-*")]
 	public async Task OnPollCallback()
 	{
 		SocketMessageComponent? sm = this.Context.Interaction as SocketMessageComponent;
@@ -55,10 +49,6 @@ public class RaidPlannerModule
 		await this.schedule.Vote(sm.Data.CustomId, this.Context.Interaction.User.Id);
 		this.Save();
 
-		await sm.UpdateAsync(update =>
-		{
-			update.Components = this.schedule.BuildReply(this.Context.Interaction.User.Id);
-		});
 	}
 }
 
@@ -67,35 +57,34 @@ public class Schedule
 	public ulong ChannelId { get; set; }
 	public ulong MessageId { get; set; }
 
-	public Day Mon { get; set; } = new();
-	public Day Wed { get; set; } = new();
-	public Day Thu { get; set; } = new();
-	public Day Fri { get; set; } = new();
+	public Day Monday { get; set; } = new();
+	public Day Tuesday { get; set; } = new();
+	public Day Wednesday { get; set; } = new();
+	public Day Thursday { get; set; } = new();
+	public Day Friday { get; set; } = new();
+	public Day Saturday { get; set; } = new();
+	public Day Sunday { get; set; } = new();
 
 	public class Day
 	{
-		public HashSet<ulong> Votes700 { get; set; } = new();
+		public HashSet<ulong> Votes400 { get; set; } = new();
 		public HashSet<ulong> Votes830 { get; set; } = new();
-		public HashSet<ulong> Votes100 { get; set; } = new();
+		public void Reset()
+		{
+			this.Votes400.Clear();
+			this.Votes830.Clear();
+		}
 	}
 
 	public void Reset()
 	{
-		this.Mon.Votes700.Clear();
-		this.Mon.Votes830.Clear();
-		this.Mon.Votes100.Clear();
-
-		this.Wed.Votes700.Clear();
-		this.Wed.Votes830.Clear();
-		this.Wed.Votes100.Clear();
-
-		this.Thu.Votes700.Clear();
-		this.Thu.Votes830.Clear();
-		this.Thu.Votes100.Clear();
-
-		this.Fri.Votes700.Clear();
-		this.Fri.Votes830.Clear();
-		this.Fri.Votes100.Clear();
+		this.Monday.Reset();
+		this.Tuesday.Reset();
+		this.Wednesday.Reset();
+		this.Thursday.Reset();
+		this.Friday.Reset();
+		this.Saturday.Reset();
+		this.Sunday.Reset();
 	}
 
 	public async Task UpdateSchedule()
@@ -133,19 +122,21 @@ public class Schedule
 	{
 		Dictionary<ulong, int> voteCounts = new();
 
-		this.GetVoteCount(ref voteCounts, this.Mon);
-		this.GetVoteCount(ref voteCounts, this.Wed);
-		this.GetVoteCount(ref voteCounts, this.Thu);
-		this.GetVoteCount(ref voteCounts, this.Fri);
+		this.GetVoteCount(ref voteCounts, this.Monday);
+		this.GetVoteCount(ref voteCounts, this.Tuesday);
+		this.GetVoteCount(ref voteCounts, this.Wednesday);
+		this.GetVoteCount(ref voteCounts, this.Thursday);
+		this.GetVoteCount(ref voteCounts, this.Friday);
+		this.GetVoteCount(ref voteCounts, this.Saturday);
+		this.GetVoteCount(ref voteCounts, this.Sunday);
 
 		return voteCounts;
 	}
 
 	private void GetVoteCount(ref Dictionary<ulong, int> voteCounts, Day day)
 	{
-		this.GetVoteCount(ref voteCounts, day.Votes700);
+		this.GetVoteCount(ref voteCounts, day.Votes400);
 		this.GetVoteCount(ref voteCounts, day.Votes830);
-		this.GetVoteCount(ref voteCounts, day.Votes100);
 	}
 
 	private void GetVoteCount(ref Dictionary<ulong, int> voteCounts, HashSet<ulong> slots)
@@ -161,95 +152,40 @@ public class Schedule
 
 	public MessageComponent Build()
 	{
-		int totalVotes = 0;
-		totalVotes += this.Mon.Votes700.Count;
-		totalVotes += this.Mon.Votes830.Count;
-		totalVotes += this.Mon.Votes100.Count;
-
-		totalVotes += this.Wed.Votes700.Count;
-		totalVotes += this.Wed.Votes830.Count;
-		totalVotes += this.Wed.Votes100.Count;
-
-		totalVotes += this.Thu.Votes700.Count;
-		totalVotes += this.Thu.Votes830.Count;
-		totalVotes += this.Thu.Votes100.Count;
-
-		totalVotes += this.Fri.Votes700.Count;
-		totalVotes += this.Fri.Votes830.Count;
-		totalVotes += this.Fri.Votes100.Count;
 
 		ComponentBuilder b = new();
-		b.WithButton($"{totalVotes}", "poll-row-header", ButtonStyle.Secondary, disabled:true, row:0);
-		b.WithButton("Mon", "poll-header-mon", ButtonStyle.Secondary, disabled:true, row:0);
-		b.WithButton("Wed", "poll-header-wed", ButtonStyle.Secondary, disabled:true, row:0);
-		b.WithButton("Thu", "poll-header-thu", ButtonStyle.Secondary, disabled:true, row:0);
-		b.WithButton("Fri", "poll-header-fri", ButtonStyle.Secondary, disabled:true, row:0);
+		b.WithButton("Monday 8:30PM", "poll-header-mon", ButtonStyle.Secondary, disabled:false, row:0);
+		b.WithButton("Tuesday (Reclears)", "poll-header-tues", ButtonStyle.Secondary, disabled:true, row:0);
+		b.WithButton("Wednesday 8:30PM", "poll-header-wed", ButtonStyle.Secondary, disabled:false, row:0);
+		b.WithButton("Thursday 8:30PM", "poll-header-thu", ButtonStyle.Secondary, disabled:false, row:0);
+		b.WithButton("Friday 8:30PM", "poll-header-fri", ButtonStyle.Secondary, disabled:false, row:0);
+		b.WithButton("Saturday 4PM", "poll-header-sat4", ButtonStyle.Secondary, disabled:false, row:1);
+		b.WithButton("Saturday 8PM", "poll-header-sat8",ButtonStyle.Secondary, disabled:false, row:1);
+		b.WithButton("Sunday 4PM", "poll-header-sun4", ButtonStyle.Secondary, disabled:false, row:2);
+		b.WithButton("Sunday 8PM", "poll-header-sun8", ButtonStyle.Secondary, disabled:false, row:2);
 
-		b.WithButton("7:00", "poll-row-700", ButtonStyle.Secondary, disabled:true, row:1);
+		/*b.WithButton("7:00", "poll-row-700", ButtonStyle.Secondary, disabled:true, row:1);
 		b.WithButton("8:30", "poll-row-830", ButtonStyle.Secondary, disabled:true, row:2);
 		b.WithButton("10:00", "poll-row-100", ButtonStyle.Secondary, disabled:true, row:3);
 
-		b.WithButton($"{this.Mon.Votes700.Count}", "group-mon-700", this.Mon.Votes700.Count >= 6 ? ButtonStyle.Success : ButtonStyle.Secondary, row:1, disabled:true);
-		b.WithButton($"{this.Wed.Votes700.Count}", "group-wed-700", this.Wed.Votes700.Count >= 6 ? ButtonStyle.Success : ButtonStyle.Secondary, row:1, disabled:true);
+		b.WithButton($"{this.Monday.Votes700.Count}", "group-mon-700", this.Monday.Votes700.Count >= 6 ? ButtonStyle.Success : ButtonStyle.Secondary, row:1, disabled:true);
+		b.WithButton($"{this.Wednesday.Votes700.Count}", "group-wed-700", this.Wednesday.Votes700.Count >= 6 ? ButtonStyle.Success : ButtonStyle.Secondary, row:1, disabled:true);
 		b.WithButton($"{this.Thu.Votes700.Count}", "group-thu-700", this.Thu.Votes700.Count >= 6 ? ButtonStyle.Success : ButtonStyle.Secondary, row:1, disabled:true);
-		b.WithButton($"{this.Fri.Votes700.Count}", "group-fri-700", this.Fri.Votes700.Count >= 6 ? ButtonStyle.Success : ButtonStyle.Secondary, row:1, disabled:true);
+		b.WithButton($"{this.Friday.Votes700.Count}", "group-fri-700", this.Friday.Votes700.Count >= 6 ? ButtonStyle.Success : ButtonStyle.Secondary, row:1, disabled:true);
 
-		b.WithButton($"{this.Mon.Votes830.Count}", "group-mon-830", this.Mon.Votes830.Count >= 6 ? ButtonStyle.Success : ButtonStyle.Secondary, row:2, disabled:true);
-		b.WithButton($"{this.Wed.Votes830.Count}", "group-wed-830", this.Wed.Votes830.Count >= 6 ? ButtonStyle.Success : ButtonStyle.Secondary, row:2, disabled:true);
+		b.WithButton($"{this.Monday.Votes830.Count}", "group-mon-830", this.Monday.Votes830.Count >= 6 ? ButtonStyle.Success : ButtonStyle.Secondary, row:2, disabled:true);
+		b.WithButton($"{this.Wednesday.Votes830.Count}", "group-wed-830", this.Wednesday.Votes830.Count >= 6 ? ButtonStyle.Success : ButtonStyle.Secondary, row:2, disabled:true);
 		b.WithButton($"{this.Thu.Votes830.Count}", "group-thu-830", this.Thu.Votes830.Count >= 6 ? ButtonStyle.Success : ButtonStyle.Secondary, row:2, disabled:true);
-		b.WithButton($"{this.Fri.Votes830.Count}", "group-fri-830", this.Fri.Votes830.Count >= 6 ? ButtonStyle.Success : ButtonStyle.Secondary, row:2, disabled:true);
+		b.WithButton($"{this.Friday.Votes830.Count}", "group-fri-830", this.Friday.Votes830.Count >= 6 ? ButtonStyle.Success : ButtonStyle.Secondary, row:2, disabled:true);
 
-		b.WithButton($"{this.Mon.Votes100.Count}", "group-mon-100", this.Mon.Votes100.Count >= 6 ? ButtonStyle.Success : ButtonStyle.Secondary, row:2, disabled:true);
-		b.WithButton($"{this.Wed.Votes100.Count}", "group-wed-100", this.Wed.Votes100.Count >= 6 ? ButtonStyle.Success : ButtonStyle.Secondary, row:2, disabled:true);
+		b.WithButton($"{this.Monday.Votes100.Count}", "group-mon-100", this.Monday.Votes100.Count >= 6 ? ButtonStyle.Success : ButtonStyle.Secondary, row:2, disabled:true);
+		b.WithButton($"{this.Wednesday.Votes100.Count}", "group-wed-100", this.Wednesday.Votes100.Count >= 6 ? ButtonStyle.Success : ButtonStyle.Secondary, row:2, disabled:true);
 		b.WithButton($"{this.Thu.Votes100.Count}", "group-thu-100", this.Thu.Votes100.Count >= 6 ? ButtonStyle.Success : ButtonStyle.Secondary, row:2, disabled:true);
-		b.WithButton($"{this.Fri.Votes100.Count}", "group-fri-100", this.Fri.Votes100.Count >= 6 ? ButtonStyle.Success : ButtonStyle.Secondary, row:2, disabled:true);
+		b.WithButton($"{this.Friday.Votes100.Count}", "group-fri-100", this.Friday.Votes100.Count >= 6 ? ButtonStyle.Success : ButtonStyle.Secondary, row:2, disabled:true);
 
-		b.WithButton($"Show my votes", "poll-show-me", ButtonStyle.Primary, row:3);
-
-		return b.Build();
-	}
-
-	public MessageComponent BuildReply(ulong user)
-	{
-		ComponentBuilder b = new();
-
-		b.WithButton(null, "poll-header-row", ButtonStyle.Secondary, disabled:true, row:0, emote: Emoji.Parse(":kiss:"));
-		b.WithButton("Mon", "poll-header-mon", ButtonStyle.Secondary, disabled:true, row:0);
-		b.WithButton("Wed", "poll-header-wed", ButtonStyle.Secondary, disabled:true, row:0);
-		b.WithButton("Thu", "poll-header-thu", ButtonStyle.Secondary, disabled:true, row:0);
-		b.WithButton("Fri", "poll-header-fri", ButtonStyle.Secondary, disabled:true, row:0);
-
-		b.WithButton("7:00", "poll-row-700", ButtonStyle.Secondary, disabled:true, row:1);
-		b.WithButton("8:30", "poll-row-830", ButtonStyle.Secondary, disabled:true, row:2);
-		b.WithButton("10:00", "poll-row-100", ButtonStyle.Secondary, disabled:true, row:3);
-
-		this.AddSlotButton(b, "vote-mon-700", 1, user);
-		this.AddSlotButton(b, "vote-wed-700", 1, user);
-		this.AddSlotButton(b, "vote-thu-700", 1, user);
-		this.AddSlotButton(b, "vote-fri-700", 1, user);
-
-		this.AddSlotButton(b, "vote-mon-830", 2, user);
-		this.AddSlotButton(b, "vote-wed-830", 2, user);
-		this.AddSlotButton(b, "vote-thu-830", 2, user);
-		this.AddSlotButton(b, "vote-fri-830", 2, user);
-
-		this.AddSlotButton(b, "vote-mon-100", 2, user);
-		this.AddSlotButton(b, "vote-wed-100", 2, user);
-		this.AddSlotButton(b, "vote-thu-100", 2, user);
-		this.AddSlotButton(b, "vote-fri-100", 2, user);
+		b.WithButton($"Show my votes", "poll-show-me", ButtonStyle.Primary, row:3);*/
 
 		return b.Build();
-	}
-
-	public void AddSlotButton(ComponentBuilder b, string id, int row, ulong user)
-	{
-		HashSet<ulong> set = this.GetSet(id);
-		bool userSelected = set.Contains(user);
-		b.WithButton(
-			userSelected ? "✔" : "✖",
-			id,
-			userSelected ? ButtonStyle.Primary : ButtonStyle.Secondary,
-			row:row);
 	}
 
 	public async Task Vote(string id, ulong user)
@@ -270,23 +206,23 @@ public class Schedule
 
 	public HashSet<ulong> GetSet(string id)
 	{
-		switch(id)
+		/*switch(id)
 		{
-			case "vote-mon-700": return this.Mon.Votes700;
-			case "vote-wed-700": return this.Wed.Votes700;
+			case "vote-mon-700": return this.Monday.Votes700;
+			case "vote-wed-700": return this.Wednesday.Votes700;
 			case "vote-thu-700": return this.Thu.Votes700;
-			case "vote-fri-700": return this.Fri.Votes700;
+			case "vote-fri-700": return this.Friday.Votes700;
 
-			case "vote-mon-830": return this.Mon.Votes830;
-			case "vote-wed-830": return this.Wed.Votes830;
+			case "vote-mon-830": return this.Monday.Votes830;
+			case "vote-wed-830": return this.Wednesday.Votes830;
 			case "vote-thu-830": return this.Thu.Votes830;
-			case "vote-fri-830": return this.Fri.Votes830;
+			case "vote-fri-830": return this.Friday.Votes830;
 
-			case "vote-mon-100": return this.Mon.Votes100;
-			case "vote-wed-100": return this.Wed.Votes100;
+			case "vote-mon-100": return this.Monday.Votes100;
+			case "vote-wed-100": return this.Wednesday.Votes100;
 			case "vote-thu-100": return this.Thu.Votes100;
-			case "vote-fri-100": return this.Fri.Votes100;
-		}
+			case "vote-fri-100": return this.Friday.Votes100;
+		} */
 
 		throw new NotSupportedException();
 	}
